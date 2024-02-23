dcbd6ffb8b038aadd2884123d2bf9f97f977ff3f
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2024-02-22 20:38:44 -0500

    Added "energy" audio feature in algorithm

--- mood_estimators/song_details_calc.py
+++ mood_estimators/song_details_calc.py
@@ -17,9 +17,19 @@ def process_dataframe(df):
     # for row in df.values:
         # song_info = [ for name, track_href, tempo, valance in ]
     # 19, 14, 10, 9
-    song_info = [calc_mood_from_details( row[19],row[14],row[10],row[9]) for row in df.values]
+    song_info = [calc_mood_from_details( row[19],row[14],row[10],row[9], row[1]) for row in df.values]
 
-def calc_mood_from_details(name, track_href, tempo, valance):
+def mood():
+    pass
+
+def scale_tempo(tempo):
+    # print(((0.0004 * (tempo - 80 ) ** 2) * 50) + 1)
+    return ((0.0004 * (tempo - 80 ) ** 2) * 50) + 1
+def scale_energy(energy):
+    return ((5 * (energy * -0.5) ** 2) * 5) + 1
+ 
+# 70-90 bpm is the range where it is unclear that a song is happy or sad
+def calc_mood_from_details(name, track_href, tempo, valance, energy):
     emotions = {
     "Happiness": 0,
     "Suprise": 0,
@@ -34,28 +44,63 @@ def calc_mood_from_details(name, track_href, tempo, valance):
     }
 
     if (tempo > 120):
-        emotions["Happiness"] += 5
-        emotions["Suprise"] += 5
-        emotions["Sadness"] -= 5
-        feelings["Tension"] += 5
-        feelings["Expressiveness"] += 4
+        emotions["Happiness"] += scale_tempo(tempo)
+        emotions["Suprise"] += scale_tempo(tempo)
+        emotions["Sadness"] -= scale_tempo(tempo)
+        feelings["Tension"] += scale_tempo(tempo)
+        feelings["Expressiveness"] += scale_tempo(tempo)
 
     elif (tempo >= 90 and tempo <= 120):
-        emotions["Happiness"] += 3
-        emotions["Suprise"] += 3
-        emotions["Sadness"] -= 3
-        feelings["Tension"] += 3
-        feelings["Expressiveness"] += 2
-
+        emotions["Happiness"] += scale_tempo(tempo)
+        emotions["Suprise"] += scale_tempo(tempo)
+        emotions["Sadness"] -= scale_tempo(tempo)
+        feelings["Tension"] += scale_tempo(tempo)
+        feelings["Expressiveness"] += scale_tempo(tempo)
+    # threshold between positive and negative emotions & feeling
     elif (tempo < 90 and tempo > 60):
-        emotions["Happiness"] -= 3
-        emotions["Suprise"] += 2
-        emotions["Sadness"] += 3
-        feelings["Tension"] += 2
-        feelings["Expressiveness"] += 1
+        emotions["Happiness"] -= scale_tempo(tempo)
+        emotions["Suprise"] -= scale_tempo(tempo)
+        emotions["Sadness"] += scale_tempo(tempo)
+        feelings["Tension"] += scale_tempo(tempo)
+        feelings["Expressiveness"] += scale_tempo(tempo)
+    
+    else: 
+        emotions["Happiness"] -= scale_tempo(tempo)
+        emotions["Suprise"] -= scale_tempo(tempo)
+        emotions["Sadness"] += scale_tempo(tempo)
+        feelings["Tension"] += scale_tempo(tempo)
+        feelings["Expressiveness"] += scale_tempo(tempo)
 
-    # print(name, feelings)
-    if (valance > .5):
+    # 
+    if (energy > 0.9):
+        emotions["Happiness"] += scale_energy(energy)
+        emotions["Suprise"] += scale_energy(energy)
+        emotions["Sadness"] -= scale_energy(energy)
+        feelings["Tension"] += scale_energy(energy)
+        feelings["Expressiveness"] += scale_energy(energy)
+
+    elif (energy > .75):
+        emotions["Happiness"] += scale_energy(energy)
+        emotions["Suprise"] += scale_energy(energy)
+        emotions["Sadness"] -= scale_energy(energy)
+        feelings["Tension"] += scale_energy(energy)
+        feelings["Expressiveness"] += scale_energy(energy)
+
+    elif (energy > .50):
+        emotions["Happiness"] -= scale_energy(energy)
+        emotions["Suprise"] -= scale_energy(energy)
+        emotions["Sadness"] += scale_energy(energy)
+        feelings["Tension"] -=scale_energy(energy)
+        feelings["Expressiveness"] -= scale_energy(energy)
+
+    else:
+        emotions["Happiness"] -= scale_energy(energy)
+        emotions["Suprise"] -= scale_energy(energy)
+        emotions["Sadness"] += scale_energy(energy)
+        feelings["Tension"] -=scale_energy(energy)
+        feelings["Expressiveness"] -= scale_energy(energy)
+
+    if (valance > 0.5):
         if (max(emotions, key=emotions.get) == "Happiness"):
             print(name + ": Expressive")
         else:
@@ -65,6 +110,9 @@ def calc_mood_from_details(name, track_href, tempo, valance):
             print(name + ": Stressing")
         else:
             print(name + ": Boring")
+
+    # print(emotions, "\n")
+    
 
 
 if os.path.exists(file_path):
