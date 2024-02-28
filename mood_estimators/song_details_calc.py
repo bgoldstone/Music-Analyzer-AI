027ed3a7b88abcba494edbcafeb126d2e2b2ad97
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2024-02-27 22:02:19 -0500

    Added Euclidean Distance to algorithm

--- mood_estimators/song_details_calc.py
+++ mood_estimators/song_details_calc.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import numpy as np
 
 DIRECTORY = 'Daeshaun'  # Ex: "Daeshaun"
 filename = 'Anime_lofi_track_details.csv'  # "Anime_lofi_track_details.csv"
@@ -10,18 +11,50 @@ file_path = os.path.join('song_data', DIRECTORY, filename)
 # pd.set_option('display.max_rows', None)
 
 
+
+emotionCords = {
+    "Stressing": ( 120, 0.30),
+    "Boring": ( -130, 0.25),
+    "Expressionless": (-130, 0.30),
+    "Expressive": (130 ,0.70),
+    "Amusing": (90, 0.80),
+    "Relaxing": (-130, 0.70)
+}
+
+# Compute Euclidean Distance in Python
+def calc_Euc_Distance(arousal, valance):
+    P1 = np.array((arousal, valance))
+
+    shortestDist = float('inf')
+    nearestVector = ""
+
+    for emotion in emotionCords:
+        P2 = np.array(emotionCords[emotion])
+        temp = P1 - P2
+
+        euclid_dist = np.sqrt(np.dot(temp.T, temp))
+        print(euclid_dist)
+
+        if euclid_dist < shortestDist:
+            shortestDist = euclid_dist
+            nearestVector = emotion
+
+    print("Nearest emotion is ",  nearestVector, ". Distance: ",  shortestDist)
+
 def process_dataframe(df):
     # Process each DataFrame, `df.values` represent all rows in the csv file.
     # For loop is used to access each row of data in pandas dataframe
-    song_info = [calc_mood_from_details(
-        row[19], row[14], row[10], row[9], row[1]) for row in df.values]
-
+    song_info = [calc_mood_from_details( row[19], row[12], row[10], row[9], row[1] ) for row in df.values]
+    
+    for song in song_info:
+        print("Name: ", song[2])
+        calc_Euc_Distance(song[0], song[1])
 
 def scale_tempo(tempo):
     # 70-90 bpm is the range where it is unclear that a song is happy or sad based on tempo
     # Therefore, equation  output smaller values between that range
     # Outliners(60bpm or 120bpm) have exponentially higher outputs
-    return ((0.0004 * (tempo - 80) ** 2) * 50) + 1
+    return ((0.0004 * (tempo - 80) ** 2) * 30) + 1
 
 
 def scale_energy(energy):
@@ -31,43 +64,37 @@ def scale_energy(energy):
     return ((5 * (energy * - 0.50) ** 2) * 5) + 1
 
 
-def calc_mood_from_details(name, track_href, tempo, valance, energy):
+def calc_mood_from_details(name, track_id, tempo, valance, energy):
     # Energy level based on `tempo` and `energy` params
     arousal = 0
 
     # Check how tempo should affect arousal level
     if (tempo > 120):
         arousal += scale_tempo(tempo)
-        arousal -= scale_tempo(tempo)
 
     elif (tempo >= 90 and tempo <= 120):
         arousal += scale_tempo(tempo)
-        arousal -= scale_tempo(tempo)
 
     # 70-90 bpm is the range where it is unclear that a song is happy or sad based on tempo
     elif (tempo < 90 and tempo > 70):
-        arousal -= scale_tempo(tempo)
+        arousal += scale_tempo(tempo)
         arousal -= scale_tempo(tempo)
     
     else: 
         arousal -= scale_tempo(tempo)
-        arousal += scale_tempo(tempo)
+
     
     # Check how energy should affect arousal level
     if (energy > .75):
         arousal += scale_energy(energy)
-        arousal -= scale_energy(energy)
 
     elif (energy > .50):
-        arousal -= scale_energy(energy)
         arousal += scale_energy(energy)
 
     elif (energy > .25):
-        arousal -= scale_energy(energy)
-        arousal += scale_energy(energy)
+        arousal -= (scale_energy(energy))
     else:
-        arousal -= scale_energy(energy)
-        arousal += scale_energy(energy)
+        arousal -= 2 * scale_energy(energy)
 
     # Now check the valence level:
     # A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track.
@@ -75,19 +102,22 @@ def calc_mood_from_details(name, track_href, tempo, valance, energy):
     # while tracks with low valence sound more negative (e.g. sad, depressed, angry).
 
     # if valance is positive, check arousal level
+    printVal = False
     if (valance > 0.5):
-        if (arousal > 0):
-            print(name + ": Upbeat, cheery")
-        else:
-            print(name + ": Relaxing, happy")
+        if (printVal == True):
+            if (arousal > 0):
+                print(name + ": Upbeat, cheery", str(arousal), str(valance))
+            else:
+                print(name + ": Relaxing, happy", str(arousal), str(valance))
     # valance is negative, check arousal level
     else:
-        if (arousal < 0):
-            print(name + ": Stressing/Urgent")
-        else:
-            print(name + ": Relaxing, sad")
+        if (printVal == True):
+            if (arousal < 0):
+                print(name + ": Stressing/Urgent", str(arousal), str(valance))
+            else:
+                print(name + ": Relaxing, sad", str(arousal), str(valance))
 
-    return (arousal, valance)
+    return (arousal, valance, name)
 
 
 # Get the data(audio features from spotify) from the csv
