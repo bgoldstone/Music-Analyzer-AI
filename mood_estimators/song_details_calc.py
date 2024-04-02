3263d5e29c1b65c63bb64d6aa83839e2021836a0
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2024-04-02 19:10:40 -0400

    changed names of dimensions in VSM

--- mood_estimators/song_details_calc.py
+++ mood_estimators/song_details_calc.py
@@ -18,13 +18,13 @@ filename = (
 file_path = os.path.join("song_data", DIRECTORY, filename)
 
 song_info = []
-stand_vect = [({'happy': 19.722225599999994, 'sad': -19.722225599999994, 'intense': 143.98899992674365, 'mild': -143.98899992674365, 'danceability': 0.647}, 'Happy - From "Despicable Me 2"', '60nZcImufyMA1MKQY3dcCH'), ({'happy': 2.0000000000000052e-07, 'sad': -2.0000000000000052e-07, 'intense': -5.0613370864, 'mild': 5.0613370864, 'danceability': 0.468}, "Cruel Angel's Thesis but is it okay if it's lofi?", '221o9DvmsX6zOIG8BbP3nz'), ({'happy': -5.006553600000000017, 'sad': 5.006553600000000017, 'intense': 10.637991392150001, 'mild': -10.637991392150001, 'danceability': 0.447}, 'Unholy Confessions', '78XFPcFYN8YFOHjtVwnPsl'), ({'happy': -7.451940799999999, 'sad': 7.451940799999999, 'intense': -1.0585302068395999, 'mild': 1.0585302068395999, 'danceability': 0.467}, 'Everybody Hurts', '6PypGyiu0Y2lCDBN1XZEnP')]
+stand_vect = [({'positive': 19.722225599999994, 'negative': -19.722225599999994, 'intense': 143.98899992674365, 'mild': -143.98899992674365, 'danceability': 0.647}, 'Happy - From "Despicable Me 2"', '60nZcImufyMA1MKQY3dcCH'), ({'positive': 2.0000000000000052e-07, 'negative': -2.0000000000000052e-07, 'intense': -5.0613370864, 'mild': 5.0613370864, 'danceability': 0.468}, "Cruel Angel's Thesis but is it okay if it's lofi?", '221o9DvmsX6zOIG8BbP3nz'), ({'positive': -5.006553600000000017, 'negative': 5.006553600000000017, 'intense': 10.637991392150001, 'mild': -10.637991392150001, 'danceability': 0.447}, 'Unholy Confessions', '78XFPcFYN8YFOHjtVwnPsl'), ({'positive': -7.451940799999999, 'negative': 7.451940799999999, 'intense': -1.0585302068395999, 'mild': 1.0585302068395999, 'danceability': 0.467}, 'Everybody Hurts', '6PypGyiu0Y2lCDBN1XZEnP')]
 
 stand_vect_dict = {
-    "happy" : [({'happy': 19.722225599999994, 'sad': -19.722225599999994, 'intense': 143.98899992674365, 'mild': -143.98899992674365, 'danceability': 0.647}, 'Happy - From "Despicable Me 2"', '60nZcImufyMA1MKQY3dcCH')],
-    "chill": [({'happy': 2.0000000000000052e-07, 'sad': -2.0000000000000052e-07, 'intense': -5.0613370864, 'mild': 5.0613370864, 'danceability': 0.468}, "Cruel Angel's Thesis but is it okay if it's lofi?", '221o9DvmsX6zOIG8BbP3nz')],
-    "stressing": [({'happy': -5.006553600000000017, 'sad': 5.006553600000000017, 'intense': 10.637991392150001, 'mild': -10.637991392150001, 'danceability': 0.447}, 'Unholy Confessions', '78XFPcFYN8YFOHjtVwnPsl'), ({'happy': -1.6484816000000002, 'sad': 1.6484816000000002, 'intense': 17.488896165004796, 'mild': -17.488896165004796, 'danceability': 0.725}, 'Demon Slayer (Rengoku Theme)', '0eTQEpSLPnZhuahEuf1IE1')],
-    "sad": [({'happy': -7.451940799999999, 'sad': 7.451940799999999, 'intense': -1.0585302068395999, 'mild': 1.0585302068395999, 'danceability': 0.467}, 'Everybody Hurts', '6PypGyiu0Y2lCDBN1XZEnP'), ({'happy': -0.0009826000000000025, 'sad': 0.0009826000000000025, 'intense': 86.75278244360685, 'mild': -86.75278244360685, 'danceability': 0.652}, 'Let Me Down Slowly', '2qxmye6gAegTMjLKEBoR3d'), ({'happy': -6.3108992, 'sad': 6.3108992, 'intense': -0.1848024869664003, 'mild': 0.1848024869664003, 'danceability': 0.418}, 'Stay With Me', '5Nm9ERjJZ5oyfXZTECKmRt')],
+    "positive" : [({'positive': 19.722225599999994, 'negative': -19.722225599999994, 'intense': 143.98899992674365, 'mild': -143.98899992674365, 'danceability': 0.647}, 'Happy - From "Despicable Me 2"', '60nZcImufyMA1MKQY3dcCH')],
+    "chill": [({'positive': 2.0000000000000052e-07, 'negative': -2.0000000000000052e-07, 'intense': -5.0613370864, 'mild': 5.0613370864, 'danceability': 0.468}, "Cruel Angel's Thesis but is it okay if it's lofi?", '221o9DvmsX6zOIG8BbP3nz')],
+    "stressing": [({'positive': -5.006553600000000017, 'negative': 5.006553600000000017, 'intense': 10.637991392150001, 'mild': -10.637991392150001, 'danceability': 0.447}, 'Unholy Confessions', '78XFPcFYN8YFOHjtVwnPsl'), ({'positive': -1.6484816000000002, 'negative': 1.6484816000000002, 'intense': 17.488896165004796, 'mild': -17.488896165004796, 'danceability': 0.725}, 'Demon Slayer (Rengoku Theme)', '0eTQEpSLPnZhuahEuf1IE1')],
+    "negative": [({'positive': -7.451940799999999, 'negative': 7.451940799999999, 'intense': -1.0585302068395999, 'mild': 1.0585302068395999, 'danceability': 0.467}, 'Everybody Hurts', '6PypGyiu0Y2lCDBN1XZEnP'), ({'positive': -0.0009826000000000025, 'negative': 0.0009826000000000025, 'intense': 86.75278244360685, 'mild': -86.75278244360685, 'danceability': 0.652}, 'Let Me Down Slowly', '2qxmye6gAegTMjLKEBoR3d'), ({'positive': -6.3108992, 'negative': 6.3108992, 'intense': -0.1848024869664003, 'mild': 0.1848024869664003, 'danceability': 0.418}, 'Stay With Me', '5Nm9ERjJZ5oyfXZTECKmRt')],
 }
 
 
@@ -39,8 +39,8 @@ def process_data(df):
     danceability = df["danceability"]
 
     emotion_dimensions = {
-    "happy": 0,
-    "sad": 0,
+    "positive": 0,
+    "negative": 0,
     "intense": 0,
     "mild": 0,
     "danceability": 0,
@@ -52,20 +52,20 @@ def process_data(df):
     song_info.append(calc_mood_from_details(float(tempo), float(valence), float(energy), track_name, track_id, emotion_dimensions))
 
 def scale_tempo(tempo):
-    # 70-90 bpm is the range where it is unclear that a song is happy or sad based on tempo
+    # 70-90 bpm is the range where it is unclear that a song is positive or negative based on tempo
     # Therefore, equation  output smaller values between that range
     # Outliners(60bpm or 120bpm) have exponentially higher outputs
     return 0.0004 * (tempo - 90) ** 3
 
 
 def scale_energy(energy):
-    # 0.40 - 0.60 energy level is the range where it is unclear that a song is happy or sad
+    # 0.40 - 0.60 energy level is the range where it is unclear that a song is positive or negative
     # Therefore, equation  output smaller values between that range
     # Outliners(0.10 or 0.9) have exponentially higher outputs
     return (5 * (energy - 0.50) ** 3) * 40
 
 def scale_valence(valence):
-    # 0.40 - 0.60 energy level is the range where it is unclear that a song is happy or sad
+    # 0.40 - 0.60 energy level is the range where it is unclear that a song is positive or negative
     # Therefore, equation  output smaller values between that range
     # Outliners(0.10 or 0.9) have exponentially higher outputs
     return (5 * (valence - 0.50) ** 3) * 40
@@ -73,9 +73,9 @@ def scale_valence(valence):
 
 def calc_mood_from_details(tempo, valence, energy, name, track_id, vectors):
     # Incorporating valence into mood vectors:
-    # Increase the "happy" vector component and decrease the "sad" vector component based on valence level.
-    vectors["happy"] += round(scale_valence(valence), 3)
-    vectors["sad"] -= round(scale_valence(valence), 3)
+    # Increase the "positive" vector component and decrease the "negative" vector component based on valence level.
+    vectors["positive"] += round(scale_valence(valence), 3)
+    vectors["negative"] -= round(scale_valence(valence), 3)
     #
     # Incorporating energy into mood vectors:
     # Increase the "intense" vector component and decrease the "mild" vector component based on energy level.
@@ -83,7 +83,7 @@ def calc_mood_from_details(tempo, valence, energy, name, track_id, vectors):
     vectors["mild"] -= round(scale_energy(energy) , 3)
     #
     # Incorporating tempo into mood vectors:
-    # Increase the "intense" vector component if tempo is high, else adjust "sad" based on the absolute value of scaled tempo.
+    # Increase the "intense" vector component if tempo is high, else adjust "negative" based on the absolute value of scaled tempo.
     vectors["intense"] += round(scale_tempo(tempo), 3) 
     vectors["mild"] -= round(scale_tempo(tempo), 3)
 
