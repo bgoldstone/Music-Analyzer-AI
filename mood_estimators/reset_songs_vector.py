bc24263b359b6dd5211484994634e3816760acdd
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2024-04-10 19:23:10 -0400

    New file to reset song vectors

--- mood_estimators/reset_songs_vector.py
+++ mood_estimators/reset_songs_vector.py
@@ -7,20 +7,14 @@ import matplotlib
 import matplotlib.pyplot as plt
 import bertai
 
-DIRECTORY = "ben"  # Ex: "Daeshaun"
+DIRECTORY = "Daeshaun"  # Ex: "Daeshaun"
 filename = (
-    "songs-for-events_track_details.json"  # "Lofi_Anime_Openings_track_details.json"
+    "Standard_songs_track_details.json"  # "Lofi_Anime_Openings_track_details.json"
 )
+
 file_path = os.path.join("song_data", DIRECTORY, filename)
 
 song_info = []
-
-stand_vect_dict = {
-    "positive" : [({'positive': 19.722225599999994, 'negative': -19.722225599999994, 'intense': 143.98899992674365, 'mild': -143.98899992674365, 'danceability': 0.647}, 'Happy - From "Despicable Me 2"', '60nZcImufyMA1MKQY3dcCH')],
-    "chill": [({'positive': 2.0000000000000052e-07, 'negative': -2.0000000000000052e-07, 'intense': -5.0613370864, 'mild': 5.0613370864, 'danceability': 0.468}, "Cruel Angel's Thesis but is it okay if it's lofi?", '221o9DvmsX6zOIG8BbP3nz')],
-    "stressing": [({'positive': -5.006553600000000017, 'negative': 5.006553600000000017, 'intense': 10.637991392150001, 'mild': -10.637991392150001, 'danceability': 0.447}, 'Unholy Confessions', '78XFPcFYN8YFOHjtVwnPsl'), ({'positive': -1.6484816000000002, 'negative': 1.6484816000000002, 'intense': 17.488896165004796, 'mild': -17.488896165004796, 'danceability': 0.725}, 'Demon Slayer (Rengoku Theme)', '0eTQEpSLPnZhuahEuf1IE1')],
-    "negative": [({'positive': -7.451940799999999, 'negative': 7.451940799999999, 'intense': -1.0585302068395999, 'mild': 1.0585302068395999, 'danceability': 0.467}, 'Everybody Hurts', '6PypGyiu0Y2lCDBN1XZEnP'), ({'positive': -0.0009826000000000025, 'negative': 0.0009826000000000025, 'intense': 86.75278244360685, 'mild': -86.75278244360685, 'danceability': 0.652}, 'Let Me Down Slowly', '2qxmye6gAegTMjLKEBoR3d'), ({'positive': -6.3108992, 'negative': 6.3108992, 'intense': -0.1848024869664003, 'mild': 0.1848024869664003, 'danceability': 0.418}, 'Stay With Me', '5Nm9ERjJZ5oyfXZTECKmRt')],
-}
 
 def process_data(df):
     # Process each DataFrame. `df` is a dictionary of the song's properties. Ex: {"danceability": 0.647, "energy": 0.822,..."album_name": "G I R L"}.
@@ -87,23 +81,17 @@ def calc_mood_from_details(tempo, valence, energy, name, track_id, vectors):
     for eachPercent in lyrics_emotions:
         index = 0
         if index == 0:
-            vectors["positive"] += (eachPercent * 20)
+            vectors["positive"] += (16 * (eachPercent / 100))
         elif index == 1:
-            vectors["negative"] += (eachPercent * 20)
+            vectors["negative"] += (16 * (eachPercent / 100))
         elif index == 2:
-            vectors["positive"] += (eachPercent * 20)
-            vectors["negative"] += (eachPercent * 20)
+            vectors["positive"] += (16 * (eachPercent / 100))
+            vectors["negative"] += (16 * (eachPercent / 100))
         elif index == 3:
-            vectors["positive"] -= (eachPercent * 20)
-            vectors["negative"] -= (eachPercent * 20)
+            vectors["positive"] -= (16 * (eachPercent / 100))
+            vectors["negative"] -= (16 * (eachPercent / 100))
 
     return(vectors, track_id, name)
-
-def cosine_similarity(vector1, vector2):
-    dot_product = np.dot(vector1, vector2)
-    magnitude_vector1 = np.linalg.norm(vector1)
-    magnitude_vector2 = np.linalg.norm(vector2)
-    return dot_product / (magnitude_vector1 * magnitude_vector2)
 
 def main():
     # Get the data(audio features from spotify) from the json
@@ -120,17 +108,6 @@ def main():
         for song in song_info:
             print(f"Song name: {song[2]}")
             print(f"Song dimensions: {song}")
-            P1 = np.array(list(song[0].values()))
-            
-            for quadrant in stand_vect_dict:
-                sum = 0
-                print(quadrant, end=": ")
-                for each_song in stand_vect_dict[quadrant]:
-                    P2 = np.array(list(each_song[0].values()))
-                    # print(each_song[1], end=": \n")
-                    # print(cosine_similarity(P1, P2))
-                    sum += cosine_similarity(P1, P2)
-                print(sum / len(stand_vect_dict[quadrant]))
             print("-----------------------------")
 
     else:
