d27e8ca0420abe10d877db948ca57a315d604a21
Benjamin Goldstone <bgoldstone1@gmail.com>
2024-04-11 14:28:41 -0400

    updated main runner

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import multiprocessing
 import signal
 import sys
+import time
 import api.main
 import website.main
 
+running = True
 
 def main():
     """Runs the API and React servers in parallel."""
     api_server = multiprocessing.Process(target=api.main.main)
     api_server.start()
-    print("Running API Server on http://localhost:8000")
     react_server = multiprocessing.Process(target=website.main.main)
     react_server.start()
-    print("Running React Server on http://localhost:3000")
-    running = True
-
+    time.sleep(5)
     def handle_signal(signum, frame):
+        global running
         print(signum, frame)
         print("Shutting down API Server...")
         api_server.join()
         print("Shutting down React Server...")
         react_server.join()
+        running = False
         sys.exit(0)
 
     signal.signal(signal.SIGINT, handle_signal)
