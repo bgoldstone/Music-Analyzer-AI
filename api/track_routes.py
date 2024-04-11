6242830d0cfb6c17543ed3d919892dfd8803f708
Benjamin Goldstone <bgoldstone1@gmail.com>
2024-04-10 21:22:39 -0400

    added get track by track_name and artist_name

--- api/track_routes.py
+++ api/track_routes.py
@@ -9,6 +9,7 @@ from database.crud import (
     get_track,
     create_track,
     delete_track,
+    get_track_by_name,
     update_track,
 )
 
@@ -53,6 +54,17 @@ def create_new_track(track: Track, request: Request) -> Dict:
 def update_track_by_id(track_id: str, track: TrackUpdate, request: Request) -> None:
     track = update_track(track_id, track, request.app.database)
 
+@track_router.get('/{artist}/{track_name}', response_description='Get a single track by artist and track name')
+def get_track_by_name_and_artist(artist:str, track_name:str, request:Request) -> Dict:
+    track = get_track_by_name(track_name, artist, request.app.database)
+    if track is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Track {track_name} by {artist} not found",
+        )
+    track["_id"] = str(track["_id"])
+    print(track)
+    return track
 
 @track_router.delete(
     "/{track_id}",
