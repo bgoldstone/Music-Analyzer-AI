d0596a99d531f8f9f022119ca25de3b519d83b81
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2024-07-09 17:09:19 -0400

    Trying to host site

--- website/main.py
+++ website/main.py
@@ -1,11 +1,9 @@
 import os
 
-
 def main():
     app_path = os.path.join(os.path.dirname(__file__), "soundsmith")
     os.chdir(app_path)
     os.system("npm start")
-
 
 if __name__ == "__main__":
     main()
